b6c965897f5ac5a2f13da037abd50798d2a25590
Mathis <mathis.vialle@protonmail.com>
2024-04-30 17:53:32 +0200

    Finishing landing page content (WIP)

--- src/app/Components/landing-page-content/landing-page-content.component.ts
+++ src/app/Components/landing-page-content/landing-page-content.component.ts
@@ -1,17 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page-content',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './landing-page-content.component.html',
   styleUrl: './landing-page-content.component.scss',
 })
 export class LandingPageContentComponent implements OnInit {
-  lpLeftTitle!: string;
+  lpSideTitle!: string;
+  lpSideTitleTwo!: string;
+  contactForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.lpLeftTitle = 'Recent projects';
+    this.initTitles();
+    this.initForm();
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+
+  private initForm() {
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  private initTitles() {
+    this.lpSideTitle = 'Recent projects';
+    this.lpSideTitleTwo = "Let's work together";
   }
 }
