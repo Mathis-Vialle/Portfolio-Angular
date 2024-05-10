8264677e04a8f0f0d58e78422953c8e857c03572
Mathis <mathis.vialle@protonmail.com>
2024-05-10 18:27:24 +0200

    removed unused elements from service

--- src/app/services/landing-page.service.ts
+++ src/app/services/landing-page.service.ts
@@ -10,18 +10,7 @@ import { ProjectListItem } from '../models/project-list-item.model';
 export class LandingPageServices {
   constructor(private http: HttpClient) {}
 
-  private _loading$ = new BehaviorSubject<boolean>(false);
-  get loading$(): Observable<boolean> {
-    return this._loading$.asObservable();
-  }
-
-  private setLoadingStatus(loading: boolean) {
-    return this._loading$.next(loading);
-  }
-
   getProjectList(): Observable<ProjectListItem[]> {
-    this.setLoadingStatus(true);
-
     return this.http.get<ProjectListItem[]>(environment.API_PROJECTS);
   }
 }
